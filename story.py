3c385b00ff5cbc54652b19b47a01ff15b4da5802
mcchuu <145382099+mcchuu@users.noreply.github.com>
2024-03-06 13:57:22 -0500

    Add files via upload

---
+++ story.py
@@ -0,0 +1,41 @@
+import random
+import time
+
+hp = 100
+sanity = 100
+money = 20
+hour = '7:30'
+
+inventory = []
+
+def home(hour):
+
+def city():
+def store():
+
+
+# def home():
+#
+#     print(f'''\n{hour}
+#         It was the middle of summer and you are playing Fortnite with your friends on a call.
+#         Your mother had already left for work that afternoon which meant you are all by yourself for the night. ''')
+#
+#     print('''\n             7:43pm
+#
+#
+#     One missed call from mom
+#     One unread message from mom
+#
+#     ''')
+#
+#     print('You tap on the message')
+#
+#     print('''\n             mom
+#
+#
+#    Please remember to get the items on the grocery list before I get home.’
+#                                                                         6:00pm
+#
+#
+# ''')
+#
