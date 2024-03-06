16446c66d82fbfc939a684c6842313cb3d592e66
oareyna <116768925+oareyna@users.noreply.github.com>
2024-03-06 12:31:45 -0500

    Add files via upload

---
+++ proj.py
@@ -0,0 +1,16 @@
+import os
+import time
+import random
+# import keyboard
+
+player_name = input("Who are ya? ")
+
+time.sleep(0.5)
+os.system('cls')
+print(f'In a world...where groceries must be purchased, only {player_name} can buy them!',end="")
+input()
+os.system('cls')
+print("unfortunately, you live in new york. good luck bozo")
+time.sleep(2)
+input("Hit enter to start! ")
+os.system('cls')
