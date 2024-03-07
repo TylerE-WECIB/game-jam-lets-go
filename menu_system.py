abfb7490ba3696219cfada04fefa7fb1ce482d6a
TylerE-WECIB <trellis1@my.waketech.edu>
2024-03-07 11:20:39 -0500

    updated stuff. added dialogue menu setup and travel menu setup. need to do code now

--- menu_system.py
+++ menu_system.py
@@ -1,23 +1,28 @@
 #import keyboard
 import time
 import os
-# need to implement different menu types. travel, combat, and dialogue
-#os.system('mode 10, 100')
-print(os.get_terminal_size())
 
-box = """╔═══════════════╦══════════════════════════════════════════════════════════════════════════════════════════════════════╗
-║ PETER GRIFFIN ║                                                                                                      ║
-╚═══════════════╩══════════════════════════════════════════════════════════════════════════════════════════════════════╝
-╔════════════════╦════════════════════════════════════════════════════════════════════════════════╦════════════════════╗
-║   .-------.    ║                                        ..                                      ║ HEALTH:   10       ║
-║   | 0   0 |    ║                                                                                ║ MONEY:    100      ║
-║  (|    >  |    ║                                                                                ║ SANITY:   BAD      ║
+player_health = 1000
+player_money = 100
+player_sanity = "good :)"
+player_time = "7:00 PM"
+# need to implement different menu types. travel, combat, and dialogue
+player_name = "asdsadsadsdasdsaaddsd"
+menu_name = f"""╔═{'═'*len(player_name)}═╗
+║ {player_name.upper()} ║
+╚═{'═'*len(player_name)}═╝"""
+menu_dialogue = f"""╔════════════════╦════════════════════════════════════════════════════════════════════════════════╦════════════════════╗
+║   .-------.    ║                                        ..                                      ║ HEALTH:   {str(player_health) + " "*(9-len(str(player_health)))}║
+║   | 0   0 |    ║                                                                                ║ MONEY:    {str(player_money) + " "*(9-len(str(player_money)))}║
+║  (|    >  |    ║                                                                                ║ SANITY:   {str(player_sanity) + " "*(9-len(str(player_sanity)))}║
 ║   |  ___  /    ║                                                                                ║ DRIP:     NONE     ║
-║    |_____/     ║                                                                                ║ TIME:     7:00 PM  ║
+║    |_____/     ║                                                                                ║ TIME:     {str(player_time) + " "*(9-len(str(player_time)))}║
 ╚════════════════╩════════════════════════════════════════════════════════════════════════════════╩════════════════════╝"""
+
 
 
 def draw_menu_dialogue(line, text):
     print("hi")
-print(box)
-print('hi')
+print(menu_name)
+print(menu_dialogue)
+
