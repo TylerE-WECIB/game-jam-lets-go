f575e58b9c2c573594f8d3001b71027c2cc2b49d
mcchuu <145382099+mcchuu@users.noreply.github.com>
2024-03-08 13:43:54 -0500

    Add files via upload

---
+++ mother.py
@@ -0,0 +1,124 @@
+import os
+
+aa="You have finally made it back home."
+ae="You are about to give the groceries back to your mother"
+
+a="But she looks a bit different."
+bb=f"""    ╔══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+    ║  YOUR HOUSE: MOM!?                                                                                                   ║
+    ║                                                                  <-__--------______                                  ║
+    ║                                                                  _____‾‾‾‾‾-->     ‾‾‾‾--_                           ║
+    ║                                                         __-- ‾ ‾       ____________       |                          ║
+    ║                                                       /‾  ___---‾‾‾‾‾‾‾            ‾‾‾‾‾‾‾                           ║
+    ║                                                      |  \\‾__------‾‾‾‾‾‾‾‾‾---_                                      ║
+    ║                                                       \\  /                      \\        _                           ║
+    ║                                                       /                   ___     \\    /  |                          ║
+    ║                                                      |                   |    \\ _-------__|                          ║
+    ║                                                      |                   \\  _ ‾           |                          ║
+    ║                                                      \\            __‾‾\\   \\/    ‾-__    / |                          ║
+    ║                                                       \\      ___/‾     \\   -_     O ‾    O |   /                     ║
+    ║                                                     /  |_\\_ <____‾‾-_ /      ‾‾--__        _\\/                       ║
+    ║                                                     |/\\_\\\\  ‾-__ \\/\\/         ___  ‾‾--___ |_|---‾                   ║
+    ║                                                                 ‾‾‾‾---------\\    |  ‾‾   /                          ║
+    ║                                                                              /|/\\/\\|    /                            ║
+    ║                                                                                                                      ║
+    ║                                                                                                                      ║
+    ╚══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝"""
+
+cc="You:You look a bit different"
+dd="Mother:hiss"
+de="Hiss"
+ee="You:Good talk, im gonna go now."
+ef="""With your lack of sanity and extreme shock, you too have decided to 
+      become a rat. 
+"""
+
+
+et="""
+   (\,/)    __QQ
+    0      (_)_">  
+(_ | |    _)    
+   / |
+                                                                                                                                                                                                 
+"""
+fin="""
+The End
+"""
+
+have="You:I have your groceries"
+been="Mother:Where have you been all this time?!!!"
+
+fight="You:Well I just had to fight some rats, and punch a couple of guys."
+
+questions="""
+...
+"""
+
+doctor="""
+
+            .----.
+           ===(_)==   I'll make you better,SweetCheeks
+          // 6  6 ||  /
+          (    7   )
+           \ '--' /
+            \_ ._/
+           __)  (__
+        /"`/`\`V/`\`
+       /   \  `Y _/_ |
+      / [DR]\_ |/ / /|
+      |     ( \/ / / /
+       \  \  \      /
+        \  `-/`  _.`
+         `=. `=./
+            `"`
+
+
+"""
+home="You now live in the psych ward."
+def ending_1():
+    os.system('cls')
+    print(aa)
+    input()
+    print(ae)
+    input()
+    print(bb)
+    input()
+    print(cc)
+    input()
+    print(dd)
+    input()
+    print(de)
+    input()
+    print(de)
+    input()
+    print(ee)
+    input()
+    os.system('cls')
+    print(ef)
+    input()
+    print(et)
+    input()
+    print(fin)
+
+def ending_2():
+    os.system('cls')
+    print(aa)
+    input()
+    print(ae)
+    input()
+    print(have)
+    input()
+    print(been)
+    input()
+    print(fight)
+    input()
+    print(questions)
+    input()
+    os.system('cls')
+    print(home)
+    input()
+
+    print(doctor)
+    print(fin)
+
+ending_2()
