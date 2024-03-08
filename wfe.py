c709503aaadf820adec0604d73b6dfeaa2dba65b
oareyna <116768925+oareyna@users.noreply.github.com>
2024-03-08 06:22:27 -0500

    Add files via upload

---
+++ wfe.py
@@ -0,0 +1,14 @@
+import os
+# import time
+
+while True:
+
+    # os.system('cls')
+    p= input("Pick a correct number between 1-10:")
+
+    if p=="3":
+        print("Yes!!!")
+        break
+    else:
+        print("Nope, loser:")
+        # time.sleep(2)
