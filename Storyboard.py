b3885016f691efae8c6552d8441d75079e5774ac
mcchuu <145382099+mcchuu@users.noreply.github.com>
2024-03-08 13:42:40 -0500

    Add files via upload

--- Storyboard.py
+++ Storyboard.py
@@ -2,7 +2,6 @@ import os
 import time
 import random
 import proj
-import mother
 
 
 def main():
@@ -21,6 +20,7 @@ def main():
     print('')
     scene1(player_health, player_sanity, player_name, player_inventory, player_money)
     scene2(player_health, player_sanity, player_inventory, player_name)
+
 
 def inventory_ui(inventory, health, money):
     print(inventory)
@@ -230,8 +230,8 @@ def scene1(health, sanity, name, inventory, money):
             |        1 Text Message                 |
             |                                       |
             ╰───────────────────────────────────────╯
-            
-            
+
+
             ''')
     time.sleep(1)
 
@@ -474,6 +474,131 @@ def scene2(health, sanity, inventory, name):
         time.sleep(.2)
     print('\n')
 
+    aa = "You have finally made it back home."
+    ae = "You are about to give the groceries back to your mother"
+
+    a = "But she looks a bit different."
+    bb = f"""    ╔══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+        ║  YOUR HOUSE: MOM!?                                                                                                   ║
+        ║                                                                  <-__--------______                                  ║
+        ║                                                                  _____‾‾‾‾‾-->     ‾‾‾‾--_                           ║
+        ║                                                         __-- ‾ ‾       ____________       |                          ║
+        ║                                                       /‾  ___---‾‾‾‾‾‾‾            ‾‾‾‾‾‾‾                           ║
+        ║                                                      |  \\‾__------‾‾‾‾‾‾‾‾‾---_                                      ║
+        ║                                                       \\  /                      \\        _                           ║
+        ║                                                       /                   ___     \\    /  |                          ║
+        ║                                                      |                   |    \\ _-------__|                          ║
+        ║                                                      |                   \\  _ ‾           |                          ║
+        ║                                                      \\            __‾‾\\   \\/    ‾-__    / |                          ║
+        ║                                                       \\      ___/‾     \\   -_     O ‾    O |   /                     ║
+        ║                                                     /  |_\\_ <____‾‾-_ /      ‾‾--__        _\\/                       ║
+        ║                                                     |/\\_\\\\  ‾-__ \\/\\/         ___  ‾‾--___ |_|---‾                   ║
+        ║                                                                 ‾‾‾‾---------\\    |  ‾‾   /                          ║
+        ║                                                                              /|/\\/\\|    /                            ║
+        ║                                                                                                                      ║
+        ║                                                                                                                      ║
+        ╚══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝"""
+
+    cc = "You:You look a bit different"
+    dd = "Mother:hiss"
+    de = "Hiss"
+    ee = "You:Good talk, im gonna go now."
+    ef = """With your lack of sanity and extreme shock, you too have decided to 
+          become a rat. 
+    """
+
+    et = """
+       (\,/)    __QQ
+        0      (_)_">  
+    (_ | |    _)    
+       / |
+
+    """
+    fin = """
+    The End
+    """
+
+    have = "You:I have your groceries"
+    been = "Mother:Where have you been all this time?!!!"
+
+    fight = "You:Well I just had to fight some rats, and punch a couple of guys."
+
+    questions = """
+    ...
+    """
+
+    doctor = """
+
+                .----.
+               ===(_)==   I'll make you better,SweetCheeks
+              // 6  6 ||  /
+              (    7   )
+               \ '--' /
+                \_ ._/
+               __)  (__
+            /"`/`\`V/`\`
+           /   \  `Y _/_ |
+          / [DR]\_ |/ / /|
+          |     ( \/ / / /
+           \  \  \      /
+            \  `-/`  _.`
+             `=. `=./
+                `"`
+
+
+    """
+    home = "You now live in the psych ward."
+
+    def ending_1():
+        os.system('cls')
+        print(aa)
+        input()
+        print(ae)
+        input()
+        print(bb)
+        input()
+        print(cc)
+        input()
+        print(dd)
+        input()
+        print(de)
+        input()
+        print(de)
+        input()
+        print(ee)
+        input()
+        os.system('cls')
+        print(ef)
+        input()
+        print(et)
+        input()
+        print(fin)
+
+    def ending_2():
+        os.system('cls')
+        print(aa)
+        input()
+        print(ae)
+        input()
+        print(have)
+        input()
+        print(been)
+        input()
+        print(fight)
+        input()
+        print(questions)
+        input()
+        os.system('cls')
+        print(home)
+        input()
+
+        print(doctor)
+        print(fin)
+
+    if objective:
+        ending_1()
+    else:
+        ending_2()
 
 main()
 
